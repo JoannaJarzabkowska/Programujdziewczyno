52f5cc5d9ee708aaff451a7ed50754c4e077388c
AliaMaris <joanna.jarzabkowska@gmail.com>
2017-10-21 16:40:04 +0200

    navigation

--- js/app.js
+++ js/app.js
@@ -10,4 +10,28 @@ document.addEventListener('DOMContentLoaded', function() {
     menuElement.addEventListener('mouseout', function() {
        dropdownList.style.display = 'none'; 
     });
-});
+    
+    var buttons = document.querySelectorAll('.read-more');
+
+   function showHide() {
+        var textArea = this.previousElementSibling;
+                             
+     if (textArea.style.display === 'none' || textArea.style.display === '') {
+         textArea.style.display = 'block';
+        this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+      } else {
+          textArea.style.display = 'none';
+        this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+      }
+     }
+    
+    for (var i = 0; i <= buttons.length; i++) {
+        buttons[i].addEventListener('click', showHide);
+    }
+  });
+
+var navigation = document.querySelector('nav.navbar');
+ window.addEventListener('scroll', function() {
+      navigation.style.borderBottom = '1px solid #7f7f7f';
+    });
+
